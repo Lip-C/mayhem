b123b5f3115a0bcfee4e5d71cafc3340a502e18f
Anthony Romrell <anthonyromrell@mac.com>
2021-01-21 14:52:07 -0700

    Work on Truck Config

---
+++ MeyhemTrucks/Assets/Scripts/TruckConfig.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TruckConfig : ScriptableObject
+{
+    public Sprite 
+        body, 
+        frontWheel, 
+        rearWheel, 
+        backAccessory, 
+        frontAccessory, 
+        sticker;
+    
+    public struct DriveTrain
+    {
+        
+    }
+}
