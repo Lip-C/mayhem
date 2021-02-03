8c214539d695e71cf68d210c4429dbfcfa5ac5f5
Anthony Romrell <anthonyromrell@mac.com>
2021-02-03 15:12:20 -0700

    Work on Truck Colors

--- MeyhemTrucks/Assets/LookAndStretch.cs
+++ MeyhemTrucks/Assets/LookAndStretch.cs
@@ -3,12 +3,10 @@ using UnityEngine;
 
 public class LookAndStretch : MonoBehaviour
 {
-
-    public SpringJoint2D spJ;
     public Transform t2;
     private void Update()
     {
-        var dist = Vector3.Distance(transform.position, t2.position);
+        var dist = Vector3.Distance(transform.position, t2.position) * 0.1f;
         var transform1 = transform;
         var transformLocalScale = transform1.localScale;
         transformLocalScale.x = dist;
